1f869641530f00d2080e76f5a99046ab7331e9e3
marwamohamed1 <39105542+marwamohamed1@users.noreply.github.com>
2018-06-05 01:34:35 +0200

    oop lesson 2

--- appendViews/app/src/main/java/com/example/android/appendviews/MainActivity.java
+++ appendViews/app/src/main/java/com/example/android/appendviews/MainActivity.java
@@ -1,9 +1,18 @@
 package com.example.android.appendviews;
 
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +31,13 @@ public class MainActivity extends AppCompatActivity {
         );
 
         myLayout.addView(sampleText);
+
+    }
+
+    public void openNotification(View view) {
+        NotificationManager notificationManager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify();
+
 
     }
 }
